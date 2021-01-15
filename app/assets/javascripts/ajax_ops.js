4134b6ce06c312d3780a0b7d11766661ebda6a0d
jrmcgarvey <jmcgarve@bellsouth.net>
2021-01-14 21:49:26 -0500

    add user update

--- app/assets/javascripts/ajax_ops.js
+++ app/assets/javascripts/ajax_ops.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', (event) => {
+function handle_ajax(event) {
   console.log('DOM fully loaded and parsed');
   const resultsDiv = document.getElementById('results-div');
   const restOpsDiv = document.getElementById('rest-ops');
@@ -6,11 +6,15 @@ document.addEventListener('DOMContentLoaded', (event) => {
   const createUserButton = document.getElementById('create-user');
   const userName = document.getElementById('user-username');
   const userPassword = document.getElementById('user-password');
-  const users_path = 'http://localhost/3001/users'
+  const updateUserButton = document.getElementById('update-user')
+  const userID = document.getElementById('user-id')
+  const userName1 = document.getElementById('user-username1')
+  const userPassword1 = document.getElementById('user-password1')
+  const users_path = 'http://localhost:3001/api/v1/users'
 
   restOpsDiv.addEventListener('click', (event) => {
     if (event.target === listUsersButton) {
-      fetch('http://localhost:3001/api/v1/users').then((response) => {
+      fetch(users_path).then((response) => {
         if (response.status === 200) {
           resultsDiv.innerHTML = '';
           response.json().then((data) => {
@@ -32,7 +36,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
         username: userName.value,
         password: userPassword.value
       }
-      fetch('http://localhost:3001/api/v1/users',
+      fetch(users_path,
         { method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify(dataObject)
@@ -54,6 +58,39 @@ document.addEventListener('DOMContentLoaded', (event) => {
           });
         }
       });
+    } else if (event.target === updateUserButton) {
+      var dataObject = {
+        username: userName1.value,
+        password: userPassword1.value
+      }
+      if (dataObject.username === "") {  // blank usernames not supported
+        delete dataObject.username;
+      }
+      if (dataObject.password === "") { // blank passwords not supported
+        delete dataObject.password;
+      }
+      fetch(`${users_path}/${userID.value}`,
+        { method: 'PUT',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(dataObject)
+        }
+      ).then((response) => {
+        if (response.status === 200) {
+          response.json().then((data) => {
+            resultsDiv.innerHTML = '';
+            let parag = document.createElement('P');
+            parag.textContent = JSON.stringify(data);
+            resultsDiv.appendChild(parag);
+          });
+        } else {
+          response.json().then((data) => {
+            alert(`Return code ${response.status} ${response.statusText} ${JSON.stringify(data)}`);
+          }).catch((error) => {
+            console.log(error);
+            alert(error);
+          });
+        }
+      });
     }
   });
-});
+}
