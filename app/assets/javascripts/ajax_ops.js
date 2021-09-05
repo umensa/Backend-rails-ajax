5c43b25f07426a754cdc72712397427d79a6bf2e
Mohamed Zouari <mensae@posteo.org>
2021-09-05 22:44:57 +0100

    Add create fact

--- app/assets/javascripts/ajax_ops.js
+++ app/assets/javascripts/ajax_ops.js
@@ -13,8 +13,12 @@ function handle_ajax(event) {
   const users_path = 'http://localhost:3001/api/v1/users';
   const deleteUserButton = document.getElementById('delete-user');
   const delUserID = document.getElementById('del-user-id');
-  const factsUser = document.getElementById('facts-user');
+  const factsUserID = document.getElementById('facts-user');
   const listUserFactsButton = document.getElementById('list-user-facts');
+  const createFactUserID = document.getElementById('create-fact-user');
+  const createFactButton = document.getElementById('create-fact');
+  const fact = document.getElementById('fact');
+  const likes = document.getElementById('likes');
 
   restOpsDiv.addEventListener('click', (event) => {
     if (event.target === listUsersButton) {
@@ -122,7 +126,7 @@ function handle_ajax(event) {
     }
     // List user's facts
     else if (event.target === listUserFactsButton) {
-      fetch(`${users_path}/${factsUser.value}/facts`,
+      fetch(`${users_path}/${factsUserID.value}/facts`,
       {
         method: 'GET',
         headers: {'Content-Type': 'application/json'}
@@ -142,6 +146,41 @@ function handle_ajax(event) {
           alert(error);
         });
       }
+      });
+    }
+    // Create a fact
+    else if (event.target === createFactButton) {
+      var dataObject = {
+        fact_text: fact.value,
+        likes: likes.value
+      }
+      if (dataObject.fact === "") {  // blank fact is not supported
+        delete dataObject.fact;
+      }
+      if (dataObject.likes === "") { // blank likes is not supported
+        delete dataObject.likes;
+      }
+      fetch(`${users_path}/${createFactUserID.value}/facts`,
+      {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(dataObject)
+      }).then((response) => {
+        if (response.status === 201) {
+          response.json().then((data) => {
+            resultsDiv.innerHTML = '';
+            let parag = document.createElement('P');
+            parag.textContent = JSON.stringify(data);
+            resultsDiv.appendChild(parag);
+          });
+        } else {
+          response.json().then((data) => {
+            alert(`Return code ${response.status} ${response.statusText} ${JSON.stringify(data)}`);
+          }).catch((error) => {
+            console.log(error);
+            alert(error);
+          });
+        }
       });
     }
   });
